e3669839b75338cd144e0657551b32540a3ad74f
gnocchi <gkussunoki@gmail.com>
2023-03-22 14:55:50 -0300

    Add BO5 functionality

--- js/script.js
+++ js/script.js
@@ -30,15 +30,88 @@ function capitalize(str) {
     return firstLetter.concat(str.slice(1).toLowerCase());
 }
 
-function playRound(playerSelection, computerSelection) {
-    const result = determineWinner(playerSelection.toLowerCase(), computerSelection);
+function playRound(playerSelection, cpuSelection, scoreboard) {
+    const result = determineWinner(playerSelection.toLowerCase(), cpuSelection);
+    const updatedScore = updateScore(scoreboard, result);
     let message;
     if (result === 1) {
-        message = `You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
+        message = `You Win! ${capitalize(playerSelection)} beats ${capitalize(cpuSelection)}`;
     } else if (result === -1) {
-        message = `You Lose! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
+        message = `You Lose! ${capitalize(cpuSelection)} beats ${capitalize(playerSelection)}`;
     } else {
-        message = `Match is a tie`;
+        message = `Both players put out ${cpuSelection}. A tie!`;
     }
-    return message;
+    return {
+        message: message,
+        scoreboard: updatedScore
+    };
 }
+
+function updateScore(scoreboard, result) {
+    let playerScore = scoreboard.playerScore;
+    let cpuScore = scoreboard.cpuScore;
+    
+    if (result === 1) {
+        playerScore++;
+    } else if (result === -1) {
+        cpuScore++;
+    }
+
+    return {
+        playerScore: playerScore,
+        cpuScore: cpuScore
+    }
+}
+
+function showFinalMessage(scoreboard, roundsRemaining) {
+    const playerScore = scoreboard.playerScore;
+    const cpuScore = scoreboard.cpuScore;
+    let message = '';
+
+    if (playerScore > cpuScore) {
+        message += 'Game Over\nPlayer Wins!';
+    } else if (cpuScore > playerScore) {
+        message += 'Game Over\nCPU Wins!';
+    } else if (roundsRemaining > 0) {
+        message += 'The match reached a stalemate.\nNo one wins!';
+    } else {
+        message += 'This match is a tie!';
+    }
+    message += `\nFinal score:\nPlayer: ${playerScore} x CPU ${cpuScore}`;
+
+    console.log(message);
+}
+
+function game() {
+    let scoreboard = {
+        playerScore: 0,
+        cpuScore: 0
+    };
+    let roundsRemaining = 5;
+
+    console.log('Game Start!');
+    for (let round = 0; round < 5; round++) {
+        const playerChoice = prompt(`Game ${round + 1} out of 5.\nPlayer:${scoreboard.playerScore} x CPU:${scoreboard.cpuScore}\nEnter your choice:`);
+        const cpuChoice = getComputerChoice();
+        const playerScore = scoreboard.playerScore;
+        const cpuScore = scoreboard.cpuScore;
+        let message;
+
+        ({message, scoreboard} = playRound(playerChoice, cpuChoice, scoreboard));
+        console.log(message);
+        roundsRemaining--;
+
+        // check for stalemate
+        if (roundsRemaining + playerScore < 3 && roundsRemaining + cpuScore < 3) {
+            break;
+        }
+
+        //check for win
+        if (scoreboard.playerScore > 2 || scoreboard.cpuScore > 2) {
+            break;
+        }
+    }
+    showFinalMessage(scoreboard, roundsRemaining);
+}
+
+game();
