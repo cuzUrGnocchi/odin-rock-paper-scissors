5955894016f3724db9e9fac2162f5b25cc6043a6
gnocchi <gkussunoki@gmail.com>
2023-03-28 19:13:44 -0300

    Add user interface

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 const buttons = document.querySelectorAll('button');
+const playerChoiceSpan = document.querySelector('.player-choice');
+const playerScoreSpan = document.querySelector('.player-score');
+const cpuChoiceSpan = document.querySelector('.cpu-choice');
+const cpuScoreSpan = document.querySelector('.cpu-score');
+const messageSpan = document.querySelector('.message');
+const roundCounterSpan = document.querySelector('.round-counter');
 
-function getComputerChoice() {
+const gameState = {
+    matchHistory: [],
+    scoreboard: {
+        playerScore: 0,
+        cpuScore: 0,
+        updateScore: function(result) {
+            if (result === 1) {
+                this.playerScore++;
+            } else if (result === -1) {
+                this.cpuScore++;
+            }
+        }
+    },
+    currentRound: 1,
+    updateGameState: function(roundRecord) {
+        this.matchHistory.push(roundRecord);
+        this.currentRound++;
+    }
+}
+
+function generateComputerChoice() {
     let choice;
     const rng = Math.floor((Math.random() * 3));
     
@@ -18,8 +44,8 @@ function getComputerChoice() {
 // returns 1 if A wins, 0 on a tie and -1 if A loses
 function determineWinner(choiceA, choiceB) {
     if (choiceA === 'rock' && choiceB === 'scissors' ||
-            choiceA === 'paper' && choiceB === 'rock'  ||
-            choiceA === 'scissors' && choiceB === 'paper') {
+        choiceA === 'paper' && choiceB === 'rock'  ||
+        choiceA === 'scissors' && choiceB === 'paper') {
         return 1;
     } else if (choiceA === choiceB) {
         return 0;
@@ -27,100 +53,59 @@ function determineWinner(choiceA, choiceB) {
     return -1;
 }
 
-function capitalize(str) {
+function getUpperCase(str) {
     const firstLetter = str.charAt(0).toUpperCase();
     return firstLetter.concat(str.slice(1).toLowerCase());
 }
 
-function playRound(event, scoreboard) {
-    const playerSelection = event.currentTarget.dataset.choice.toLowerCase();
-    const cpuSelection = getComputerChoice();
-    const result = determineWinner(playerSelection.toLowerCase(), cpuSelection);
-    let updatedScore;
-    if (scoreboard) {
-        updatedScore = updateScore(scoreboard, result);
+function updateUI(message) {
+    if (typeof message !== 'string') {
+        message = '';
     }
+    const scoreboard = gameState.scoreboard;
+    const lastRound = gameState.matchHistory.slice(-1)[0];
+    const currentRound = gameState.currentRound;
+
+    if (currentRound > 1) {
+        playerChoiceSpan.textContent = getUpperCase(lastRound.playerChoice);
+        cpuChoiceSpan.textContent = getUpperCase(lastRound.cpuChoice);
+    }
+
+    playerScoreSpan.textContent = scoreboard.playerScore;
+    cpuScoreSpan.textContent = scoreboard.cpuScore;
+    messageSpan.textContent = message;
+    roundCounterSpan.textContent = `Round ${currentRound}`;
+}
+
+function playRound(event) {
+    if (gameState.scoreboard.playerScore === 5 || gameState.scoreboard.cpuScore === 5) return;
+
+    const playerChoice = event.currentTarget.dataset.choice.toLowerCase();
+    const cpuChoice = generateComputerChoice();
+    const result = determineWinner(playerChoice.toLowerCase(), cpuChoice);
     let message;
-    if (result === 1) {
-        message = `You Win! ${capitalize(playerSelection)} beats ${capitalize(cpuSelection)}.`;
-    } else if (result === -1) {
-        message = `You Lose! ${capitalize(cpuSelection)} beats ${capitalize(playerSelection)}.`;
-    } else {
-        message = `Both players put out ${cpuSelection}. A tie!`;
-    }
-    return {
-        message: message,
-        scoreboard: updatedScore
-    };
-}
 
-function updateScore(scoreboard, result) {
-    let playerScore = scoreboard.playerScore;
-    let cpuScore = scoreboard.cpuScore;
+    gameState.scoreboard.updateScore(result);
     
-    if (result === 1) {
-        playerScore++;
+    const playerScore = gameState.scoreboard.playerScore;
+    const cpuScore = gameState.scoreboard.cpuScore;
+    
+    if (playerScore === 5) {
+        message = `Game Over. You Win!`;
+    } else if (cpuScore === 5) {
+        message = `Game Over. You Lose!`;
+    } else if (result === 1) {
+        message = `You Win! ${getUpperCase(playerChoice)} beats ${getUpperCase(cpuChoice)}.`;
     } else if (result === -1) {
-        cpuScore++;
-    }
-
-    return {
-        playerScore: playerScore,
-        cpuScore: cpuScore
-    }
-}
-
-function showFinalMessage(scoreboard, roundsRemaining) {
-    const playerScore = scoreboard.playerScore;
-    const cpuScore = scoreboard.cpuScore;
-    let message = '';
-
-    if (playerScore > cpuScore) {
-        message += 'Game Over\nPlayer Wins!';
-    } else if (cpuScore > playerScore) {
-        message += 'Game Over\nCPU Wins!';
-    } else if (roundsRemaining > 0) {
-        message += 'The match reached a stalemate.\nNo one wins!';
+        message = `You Lose! ${getUpperCase(cpuChoice)} beats ${getUpperCase(playerChoice)}.`;
     } else {
-        message += 'This match is a tie!';
+        message = `Both players put out ${cpuChoice}. A tie!`;
     }
-    message += `\nFinal score:\nPlayer: ${playerScore} x CPU ${cpuScore}`;
 
-    console.log(message);
+    gameState.updateGameState({playerChoice, cpuChoice});
+    updateUI(message);
 }
+
+document.addEventListener('DOMContentLoaded', updateUI);
 
 buttons.forEach(btn => btn.addEventListener('click', playRound));
-
-/*
-function game() {
-    let scoreboard = {
-        playerScore: 0,
-        cpuScore: 0
-    };
-    let roundsRemaining = 5;
-
-    console.log('Game Start!');
-    for (let round = 0; round < 5; round++) {
-        const playerChoice = prompt(`Game ${round + 1} out of 5.\nPlayer:${scoreboard.playerScore} x CPU:${scoreboard.cpuScore}\nEnter your choice:`);
-        const cpuChoice = getComputerChoice();
-        const playerScore = scoreboard.playerScore;
-        const cpuScore = scoreboard.cpuScore;
-        let message;
-
-        ({message, scoreboard} = playRound(playerChoice, cpuChoice, scoreboard));
-        console.log(message);
-        roundsRemaining--;
-
-        // check for stalemate
-        if (roundsRemaining + playerScore < 3 && roundsRemaining + cpuScore < 3) {
-            break;
-        }
-
-        //check for win
-        if (scoreboard.playerScore > 2 || scoreboard.cpuScore > 2) {
-            break;
-        }
-    }
-    showFinalMessage(scoreboard, roundsRemaining);
-}
-*/
